fb2244c93c2748c562f9ceddec5a5c90ecf0ce43
Glitch (nostalgic-pollen-antimatter) <none>
2023-06-21 00:14:21 +0000

    added mongo and event listenining

--- app.js
+++ app.js
@@ -1,93 +1,72 @@
-// Require the necessary discord.js classes
-const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
+import DiscordOauth2 from "discord-oauth2";
+import 'dotenv/config';
+import express from 'express';
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
+} from 'discord-interactions';
+import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
+import fetch from "node-fetch";
+import { request } from 'undici';
 
-// Create a new client instance
-const client = new Client({ intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildPresences,
-] });
-
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
-
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	}
-}
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-// Log in to Discord with your client's token
-//client.login(process.env.BOT_TOKEN);
-
-///////////
-const { request } = require('undici');
-const express = require('express');
+// Create an express app
 const app = express();
+// Get port, or default to 3000
+const PORT = process.env.PORT || 3000;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-app.get('/redirect', async ({ query }, response) => {
-	const { code } = query;
+app.get('/redirect', async function (req, response) {
+  
+  console.log(req.query);
+  
+  let options = {
+    url: 'https://discord.com/api/oauth2/token',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams({
+      'client_id': process.env.APP_ID,
+      'client_secret': process.env.DISCORD_TOKEN,
+      'grant_type': 'authorization_code',
+      'code': req.query.code,
+      'redirect_uri': `https://nostalgic-pollen-antimatter.glitch.me/redirect`,
 
-	if (code) {
-		try {
-			const tokenResponseData = await request('https://discord.com/api/oauth2/token', {
-				method: 'POST',
-				body: new URLSearchParams({
-					client_id: process.env.APP_ID,
-					client_secret: process.env.DISCORD_TOKEN,
-					code,
-					grant_type: 'authorization_code',
-					redirect_uri: `https://nostalgic-pollen-antimatter.glitch.me/redirect`,
-					scope: 'identify',
-				}).toString(),
+      'scope': 'identify connections role_connections.write'
+    })
+  }
+
+  let discord_data = await fetch('https://discord.com/api/oauth2/token', options).then((response) => {
+    return response.json();
+  }).then((response) => {
+    return response;
+  });
+  
+  console.log(discord_data);
+  
+			const userResult = await request('https://discord.com/api/users/@me/connections', {
 				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded',
+					authorization: `${discord_data.token_type} ${discord_data.access_token}`,
 				},
 			});
-
-			const oauthData = await tokenResponseData.body.json();
-
-			const userResult = await request('https://discord.com/api/users/@me', {
-				headers: {
-					authorization: `${oauthData.token_type} ${oauthData.access_token}`,
-				},
-			});
-
-			console.log(await userResult.body.json());
-		} catch (error) {
-			// NOTE: An unauthorized token will not throw an error
-			// tokenResponseData.statusCode will be 401
-			console.error(error);
-		}
-	}
-
-	return response.sendFile('index.html', { root: '.' });
+  
+  console.log(await userResult.body.json());
+  
+    const applicationRoleResult = await request(`https://discord.com/api/users/@me/applications/${process.env.APP_ID}/role-connection`, {
+    headers: {
+      authorization: `${discord_data.token_type} ${discord_data.access_token}`,
+    },
+  });
+  
+  console.log(await applicationRoleResult.body.json());
+  
+  response.send('heyy');
 });
 
-app.listen(() => console.log(`App listening at http://localhost:$`));
+app.listen(PORT, () => {
+  console.log('Listening on 1port', PORT);
+});
---
+++ discordjs-app.js
@@ -0,0 +1,72 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('dotenv/config');
+const { MongoClient } = require('mongodb');
+
+
+// Create a new client instance
+const client = new Client({ intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
+] });
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+// Replace the uri string with your MongoDB deployment's connection string.a
+const uri = `mongodb+srv://cdietschrunfast:${process.env.MONGO_DB_PASSWORD}@goodplays.yhu6h4r.mongodb.net/?retryWrites=true&w=majority`;
+const mongoClient = new MongoClient(uri);
+async function run() {
+  try {
+    const database = mongoClient.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'The Room'
+    const query = { title: "The Room" };
+    const options = {
+      // sort matched documents in descending order by rating
+      sort: { "imdb.rating": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    const movie = await movies.findOne(query, options);
+    // since this method returns the matched document, not a cursor, print it directly
+    console.log(movie);
+  } finally {
+    await mongoClient.close();
+  }
+}
+run().catch(console.dir);
+
+client.login(process.env.BOT_TOKEN);

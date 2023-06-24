877bbfb0a6dc4351485b15ca6c3875612ef9203e
Chris Dietsch <cdietschrunfast@gmail.com>
2023-06-24 15:11:33 +1200

    cleanup

--- api/discordjs_app.js
+++ api/discordjs_app.js
@@ -1,6 +1,4 @@
 import 'dotenv/config';
-
-
 import fs from 'fs';
 import path from 'path';
 import { Client, Collection, GatewayIntentBits } from 'discord.js';
@@ -17,19 +15,14 @@ const client = new Client({ intents: [
 
 client.commands = new Collection();
 const foldersPath = path.join(import.meta.url, '../commands');
-//const commandFolders = fs.readdirSync(foldersPath);
 
-
-//for (const folder of commandFolders) {abcd
 fs.readdirSync(new URL('./commands', import.meta.url)).forEach((dirContent) => {
 	const commandsPath = path.join(foldersPath, dirContent);
 	const commandFiles = fs.readdirSync(new URL(commandsPath, import.meta.url)).filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
-		//const command = require(filePath);
     import(filePath).then((exported) => {
       const command = exported.default;
-      //console.log(exported.default);
 		if ('data' in command && 'execute' in command) {
 			client.commands.set(command.data.name, command);
 		} else {
@@ -40,25 +33,17 @@ fs.readdirSync(new URL('./commands', import.meta.url)).forEach((dirContent) => {
 });
 
 const eventsPath = path.join(import.meta.url, '../events');
-//const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-//for (const file of eventFiles) {
 fs.readdirSync(new URL('./events', import.meta.url)).forEach((dirContent) => {
 	const filePath = path.join(eventsPath, dirContent);
 	    
   import(filePath).then((exported) => {
       const event = exported.default;
-      console.log(exported.default);
-		
-    
     	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args));
 	}
       });
-  
-
 });
 
 client.login(process.env.BOT_TOKEN);
--- api/express_app.js
+++ api/express_app.js
@@ -2,18 +2,10 @@ import DiscordOauth2 from "discord-oauth2";
 import crypto from "crypto";
 import 'dotenv/config';
 import express from 'express';
-import {
-  InteractionType,
-  InteractionResponseType,
-  InteractionResponseFlags,
-  MessageComponentTypes,
-  ButtonStyleTypes,
-} from 'discord-interactions';
-import { VerifyDiscordRequest, DiscordRequest } from './utils.js';
+import { VerifyDiscordRequest } from './utils.js';
 import fetch from "node-fetch";
 import { request } from 'undici';
 import db from './connections/mongo.js';
-import { ObjectId } from "mongodb";
 import cors from 'cors';
 
 export async function StartExpressServer() {
@@ -24,35 +16,31 @@ export async function StartExpressServer() {
   const PORT = process.env.PORT || 9000;
   // Parse request body and verifies incoming requests using discord-interactions package1
   app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
-  const token = '';
   app.use(cors());
   
-  app.get('/data', async function (req, response) {
+  app.get('/data', async function (req, response)
+  {
     
     const userId = req.query.userId;
-    console.log(userId);
-  let collection = await db.collection("game_play");
-//   let results = await collection.aggregate([
-//     {"$project": {"gameName": 1, "startTimestamp": 1, "endTimestamp": 1, "userId": 1}},
-//     {"$sort": {"endTimestamp": -1}},
-//     {"$limit": 3}
-//   ]).toArray();
+    let collection = await db.collection("game_play");
     const query = { userId: userId };
-    const options = {
+    const options =
+    {
         sort: {endTimestamp: -1},
         projection: { gameName: 1, startTimestamp: 1, endTimestamp: 1, _id: 0}
     };
 
     let results = await collection.find(query, options).toArray();
 
-  //response.send('aaabcde');
-  response.send(results).status(200);
+    response.send(results).status(200);
   });
   
-  app.post('/token', async (request, reply) => {
+  app.post('/token', async (request, reply) =>
+  {
     const { code, client_id, redirect_uri } = request.query;
     
-    let options = {
+    let options =
+    {
       url: 'https://discord.com/api/oauth2/token',
       method: 'POST',
       headers: {
@@ -74,66 +62,10 @@ export async function StartExpressServer() {
     });
     
     reply.send(discord_data);
-    
   });
           
-           
-  app.get('/getoauthloginurl', async function (req, res) {
-     const oauth = new DiscordOauth2({
-       clientId: process.env.APP_ID,
-       clientSecret: process.env.DISCORD_TOKEN,
-       redirectUri: 'https://nostalgic-pollen-antimatter.glitch.me/redirect'
-     });
-    
-    const url = oauth.generateAuthUrl({
-      scope: ["identify"],
-      state: crypto.randomBytes(16).toString("hex"),
-    });
-    console.log(url);
-  });
-  
-  app.get('/redirect', async function (req, response) {
-
-    console.log(req.query);
-
-    let options = {
-      url: 'https://discord.com/api/oauth2/token',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-        'client_id': process.env.APP_ID,
-        'client_secret': process.env.DISCORD_TOKEN,
-        'grant_type': 'authorization_code',
-        'code': req.query.code,
-        'redirect_uri': `https://nostalgic-pollen-antimatter.glitch.me/redirect`,
-
-        'scope': 'identify'
-      })
-    }
-
-    let discord_data = await fetch('https://discord.com/api/oauth2/token', options).then((response) => {
-      return response.json();
-    }).then((response) => {
-      return response;
-    });
-
-    console.log(discord_data);
-
-        const userResult = await request('https://discord.com/api/users/@me', {
-          headers: {
-            authorization: `${discord_data.token_type} ${discord_data.access_token}`,
-          },
-        });
-
-    console.log(await userResult.body.json());
-    
-    response.send('heyy');
-  });
-
   app.listen(PORT, () => {
-    console.log('Listening on 1port', PORT);
+    console.log('Listening on port', PORT);
   });
 
 }
